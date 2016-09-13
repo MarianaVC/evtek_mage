efc091acf5ae156aa1eafbf853550d56b5e98326
root <mariana.valdivia.carbonell@gmail.com>
2016-09-13 12:35:09 -0500

    Se agrega módulo de proveedores incompleto

---
+++ app/code/local/MiradorMx/Distribuidor/Helper/Data.php
@@ -0,0 +1,18 @@
+<?php
+
+class MiradorMx_Distribuidor_Helper_Data extends Mage_Customer_Helper_Data{
+
+    /**
+     * Retrieve Wholesale customer register form url
+     *
+     * @return string
+     */
+
+    public function getWholesaleRegisterUrl()
+    {
+        return $this->_getUrl('customer/account/createwholesale');
+
+    }
+
+
+}
